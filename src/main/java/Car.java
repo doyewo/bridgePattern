ef1235177bc212565438ae65f5fdba3cd30ddba1
doyewo <dexteroyewo@gmail.com>
2019-04-07 18:55:50 -0500

    "Updates on car and vehicle class"

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,2 +1,14 @@
-public class Car {
+class Car extends Vehicle {
+    public Car(Garage garage1, Garage garage2)
+    {
+        super(garage1, garage2);
+    }
+
+    @Override
+    public void manufacture()
+    {
+        System.out.print("Car ");
+        garage1.work();
+        garage2.work();
+    }
 }
--- src/main/java/Vehicle.java
+++ src/main/java/Vehicle.java
@@ -1,2 +1,12 @@
-public class Vehicle {
+abstract class Vehicle {
+    protected Garage garage1;
+    protected Garage garage2;
+
+    protected Vehicle(Garage garage1, Garage garage2)
+    {
+        this.garage1 = garage1;
+        this.garage2 = garage2;
+    }
+
+    abstract public void manufacture();
 }
